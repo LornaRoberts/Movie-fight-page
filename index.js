55fd61e35e88e0a9c42171bb150fb6f6e1add31a
Lorna <lroberts1980@yahoo.com>
2020-05-28 18:11:28 +0100

    Completed app

--- index.js
+++ index.js
@@ -62,9 +62,9 @@ const onMovieSelect = async (movie, summaryElement, side) => {
     summaryElement.innerHTML = movieTemplate(response.data);
 
     if (side === 'left'){
-      leftMovie === response.data;
+      leftMovie = response.data;
     }else {
-      rightMovie === response.data;
+      rightMovie = response.data;
     }
 
     if (leftMovie && rightMovie){
@@ -73,6 +73,25 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 };
 
 const runComparison = () => {
+  const leftSideStats = document.querySelectorAll('#left-summary .notification');
+  const rightSideStats = document.querySelectorAll('#right-summary .notification');
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+
+    const leftSideValue= leftStat.dataset.value;
+    const rightSideValue= rightStat.dataset.value;
+
+if (rightSideValue > leftSideValue){
+  leftStat.classList.remove('is-primary');
+  leftStat.classList.add('is-warning');
+}else{
+  rightStat.classList.remove('is-primary');
+  rightStat.classList.add('is-warning');
+}
+
+
+  });
 //run the first article element for each movie
 //run a comparison of the number of awards
 //then run some styling to that 'article' award
@@ -86,6 +105,16 @@ const movieTemplate = (movieDetail) =>{
   const metascore = parseInt(movieDetail.Metascore);
   const imdbRating = parseFloat(movieDetail.imdbRating);
   const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+  const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+    const value = parseInt(word);
+    if (isNaN(value)){
+      return prev;
+    }else {
+      return prev + value;   
+     }
+  }, 0);
+
+
 return  `
   <article class="media">
     <figure class="media-left">
@@ -101,23 +130,23 @@ return  `
       </div>
     </div>
   </article>
-  <article class="notification is-primary">
+  <article data-value=${awards} class="notification is-primary">
     <p class="title">${movieDetail.Awards}</p>
     <p class="subtitle">Awards</p>
   </article>
-  <article class="notification is-primary">
+  <article data-value=${dollars} class="notification is-primary">
   <p class="title">${movieDetail.BoxOffice}</p>
   <p class="subtitle">Box Office</p>
 </article>
-<article class="notification is-primary">
+<article data-value=${metascore} class="notification is-primary">
     <p class="title">${movieDetail.Metascore}</p>
     <p class="subtitle">Metascore</p>
   </article>
-  <article class="notification is-primary">
+  <article  data-value=${imdbRating} class="notification is-primary">
     <p class="title">${movieDetail.imdbRating}</p>
     <p class="subtitle">IMDB Rating</p>
   </article>
-  <article class="notification is-primary">
+  <article data-value=${imdbVotes} class="notification is-primary">
     <p class="title">${movieDetail.imdbVotes}</p>
     <p class="subtitle">IMDB Votes</p>
   </article>
